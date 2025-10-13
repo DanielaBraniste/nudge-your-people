0942e016527690b230712e0a8f38f3c27c246b0c
DanielaBraniste <43215641+DanielaBraniste@users.noreply.github.com>
2025-10-13 16:12:06 +0300

    Create AnalyticsForm.tsx

---
+++ src/components/AnalyticsForm.tsx
@@ -0,0 +1,21 @@
+export const AnalyticsForm = () => {
+  // This hidden form is required for Netlify to detect the form at build time
+  return (
+    <form 
+      name="analytics" 
+      netlify="true" 
+      netlify-honeypot="bot-field"
+      hidden
+    >
+      <input type="hidden" name="form-name" value="analytics" />
+      <input type="hidden" name="bot-field" />
+      <input type="text" name="timeSpent" />
+      <input type="text" name="peopleAdded" />
+      <input type="text" name="catchUpDetails" />
+      <input type="text" name="pwaInstalled" />
+      <input type="text" name="device" />
+      <input type="text" name="location" />
+      <input type="text" name="timestamp" />
+    </form>
+  );
+};
